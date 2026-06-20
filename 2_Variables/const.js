4995d7dbcf7f2abcfa558a5b8c8285bbd499097f
Amit571 <amitshinde44020@gmail.com>
2026-06-20 05:31:19 +0530

    Variables Done

---
+++ 2_Variables/const.js
@@ -0,0 +1,12 @@
+const var1 = 'Amit Shinde';
+const var2 = '22';
+const var3 = 'Mumbai';
+
+//this throws error because const can not be re-assign
+const var4;
+console.log(var4)
+
+//this one is also
+var1 = 'Sumit Shinde'
+
+console.table([var1, var2, var3])
---
+++ 2_Variables/let.js
@@ -0,0 +1,6 @@
+let var1 = 'Amit Shinde'
+let var2 = '22'
+let var3 = 'Mumbai'
+let var4;
+
+console.table([var1, var2, var3, var4])
