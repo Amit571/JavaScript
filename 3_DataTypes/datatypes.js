f1500d82121d8bd19d566eb5431f5db6a949aec8
Amit571 <amitshinde44020@gmail.com>
2026-06-20 08:29:56 +0530

    Pemeative data types done

---
+++ 3_DataTypes/datatypes.js
@@ -0,0 +1,37 @@
+// Permeative Data Types --
+// String =>
+let name = 'Amit Shinde';
+console.log(name);
+console.log(typeof name);
+
+// Number =>
+const age = 23;
+console.log(age);
+console.log(typeof age);
+
+// BigInt =>
+const customerId = 123456789012345678901234567890n
+console.log(customerId);
+console.log(typeof customerId);
+
+// Null =>
+let city = null;
+console.log(city);
+console.log(typeof city)
+// Null is an value.
+// When we get type of null it always return object.
+
+// Undefine =>
+let state;
+console.log(state)
+console.log(typeof state)
+
+// Boolean =>
+let isLogin = true
+console.log(isLogin)
+console.log(typeof isLogin)
+
+//Symbol =>
+let unId = Symbol('uid458ik')
+console.log(unId)
+console.log(typeof unId)
