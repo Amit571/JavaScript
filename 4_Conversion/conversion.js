479d7ef23f90e123354f3eed939c66990b39cc34
Amit571 <amitshinde44020@gmail.com>
2026-06-22 10:00:51 +0530

    conversion

---
+++ 4_Conversion/conversion.js
@@ -0,0 +1,15 @@
+let var1 = '87'
+let conVar1 = Number(var1)
+console.table([typeof(var1), conVar1, typeof(conVar1)])
+
+let var1a = '76h'
+let conVar1a = Number(var1a)
+console.table([typeof(var1a), conVar1a, typeof(conVar1a)])
+
+let var2 = 75
+let conVar2 = String(var2)
+console.table([typeof(var2), conVar2, typeof(conVar2)])
+
+let var3 = 0
+let conVar3 = Boolean(var3)
+console.table([typeof(var3), conVar3, typeof(conVar3)])
