33de7573f959c05131078149e491c29e8e0802e2
Amit571 <amitshinde44020@gmail.com>
2026-06-23 14:35:17 +0530

    Strings

---
+++ 5_Strings/strings.js
@@ -0,0 +1,13 @@
+// String Operations =>
+let str1 = 'JavaScript'
+let str2 = 'JavaScript is Good'
+console.log(typeof str1)
+console.log(str1.length)
+console.log(str1.toUpperCase())
+console.log(str1.toLocaleLowerCase())
+console.log(`the first latter '${str1[0]}' and last is '${str1[str1.length -1]}'`)
+console.log(str2.split(' '))
+console.log(str2.split(' ').reverse().join(' '))
+console.log(str1.charAt(2))
+console.log(str1.indexOf('a'))
+console.log(str1.at(4))
