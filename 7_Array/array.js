5d40a1378a21ba154ef0d9d0762755b57109ebc5
Amit571 <amitshinde44020@gmail.com>
2026-07-02 11:23:42 +0530

    Array 1

---
+++ 7_Array/array.js
@@ -0,0 +1,19 @@
+const myArray = [1,3,5,6,7]
+
+// myArray.push(10)
+// myArray.push(100)
+// console.log(myArray)
+// myArray.pop()
+// console.log(myArray)
+
+console.log(myArray.slice(1,3))
+
+console.log(myArray)
+console.log(myArray.splice(1,3))
+console.log(myArray)
+
+
+
+
+
+
